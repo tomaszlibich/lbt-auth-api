9e155dbcb650dc62cf0fe7858d25bb92bed8447f
Tomasz Libich <t.libich@gmail.com>
2025-12-30 21:03:43 +0000

    set trust proxy to true

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import authRoutes from "./routes/auth.js";
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.set("trust proxy", true);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(authRoutes);
